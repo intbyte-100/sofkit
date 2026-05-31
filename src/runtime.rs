e00cfaba34e50dcd9f153776d20313bf5e9e8164
intbyte-100 <66622928+intbyte-100@users.noreply.github.com>
2026-05-31 23:33:45 +0300

    add runtime with scope stack and widget binding

---
+++ src/runtime.rs
@@ -0,0 +1,47 @@
+use std::{cell::RefCell, ops::Deref, rc::Rc};
+
+use gtk::glib::object::{Cast, IsA};
+
+pub struct Runtime {
+    vec: RefCell<Vec<Box<dyn Scope>>>,
+}
+
+impl Runtime {
+    pub fn new() -> Self {
+        Self { vec: Default::default() }
+    }
+    
+    pub fn get() -> Rc<Runtime> {
+        RUNTIME.with(|it| it.clone())
+    }
+
+    pub fn run_with_scope<F, S: Scope + 'static>(&self, scope: S, f: F)
+    where
+        F: FnOnce(),
+    {
+        self.vec.borrow_mut().push(Box::new(scope));
+        f();
+        self.vec.borrow_mut().pop();
+    }
+    
+    pub fn with_current_scope<F>(&self, f: F)
+    where
+        F: FnOnce(&dyn Scope),
+    {
+        f(self.vec.borrow().last().map(|s| s.as_ref()).expect("Scope in not registered"))
+    }
+    
+    pub fn bind_widget(&self, widget: &impl IsA<gtk::Widget>) {
+        let widget: gtk::Widget = widget.upcast_ref().clone();
+        self.with_current_scope(|scope| scope.bind_widget(widget));
+    }
+}
+
+
+thread_local! {
+    static RUNTIME: Rc<Runtime> = Rc::from(Runtime::new());
+}
+
+pub trait Scope {
+    fn bind_widget(&self, widget: gtk::Widget);
+}
